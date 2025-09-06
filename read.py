06ec0744f089779ac7ec8010715b5086fbe739a8
unknown <mahinulhuque1996@gmail.com>
2025-09-06 21:56:28 +0600

    end

--- read.py
+++ read.py
@@ -1,10 +1,16 @@
 ipsum_file = open('files/ipsum.txt')
 
-for line in ipsum_file:
-    print(line.rstrip())
+# for line in ipsum_file:
+#     print(line.rstrip())
 
 
-ipsum_file.seek(0)
+# ipsum_file.seek(0)
 
-lines = ipsum_file.readlines()
-print(lines)
+# lines = ipsum_file.readlines()
+# print(lines)
+
+ipsum_file.seek(50)
+file_text = ipsum_file.read(100)
+print(file_text)
+
+ipsum_file.close()
