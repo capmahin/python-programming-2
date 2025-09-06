babf333facb8a88308f1b17b0dcca43243965465
unknown <mahinulhuque1996@gmail.com>
2025-09-06 13:36:09 +0600

    add decorator

---
+++ decorators.py
@@ -0,0 +1,18 @@
+def cough_dec(func):
+
+    def func_wrapper():
+        # code before fucntion
+        print('*cough*')
+        func()
+        # code after function
+        print('*cough*')
+
+    return func_wrapper    
+
+
+@cough_dec
+
+def question():
+    print('can you give me a discount on that?')
+
+question()    
