01a0cb8119dfa88d53412c0665c26b33eade80bc
unknown <mahinulhuque1996@gmail.com>
2025-09-29 23:21:08 +0600

    writelines

--- write.py
+++ write.py
@@ -5,3 +5,14 @@ with open('files/write.txt','w') as write_file:
 
 with open('files/write.txt','a') as write_file:
     write_file.write('\nI I love it so much. i dream in python')    
+
+
+quotes = [
+    '\nI can resist everythong except temptation',
+    '\nWe are all in the gutter, but some of us are looking at the stars',
+    '\nAlways forgive your enemies - nothing annoys them so much'
+]    
+
+
+with open('files/write.txt','a') as write_file:
+    write_file.writelines(quotes)    
