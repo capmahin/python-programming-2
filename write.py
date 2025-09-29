88841b01fd03402fe6733a570c13ddeec7dc5deb
unknown <mahinulhuque1996@gmail.com>
2025-09-29 23:16:08 +0600

    add another python

--- write.py
+++ write.py
@@ -1,2 +1,7 @@
 with open('files/write.txt','w') as write_file:
     write_file.write('Hey there ninjas, python is awesomw')
+
+#junk    
+
+with open('files/write.txt','a') as write_file:
+    write_file.write('\nI I love it so much. i dream in python')    
