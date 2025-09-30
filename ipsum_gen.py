48bcd7db6eb7ad28a852872bf5016f228eb0cba4
unknown <mahinulhuque1996@gmail.com>
2025-09-30 21:36:25 +0600

    add themed

---
+++ ipsum_gen.py
@@ -0,0 +1,20 @@
+from random import randint
+
+ninja_words=[
+    'Aiki','Buyu','Chimojutusu','Cho sen','Dojo','jin'
+]
+
+def ninjarize(word):
+   random_pos = randint(0, len(ninja_words)-1)
+   return f'{word}{ninja_words[random_pos]}'
+
+paragraphs = int(input('How many paragraphs of ninja ipsum:'))
+
+
+with open('ipsum.txt') as ipsum_original:
+   items = ipsum_original.read().split()
+
+   for n in range(paragraphs):
+      ninja_text = list(map(ninjarize, items))
+      with open('ninja_ipsum.txt')as ipsum_ninja:
+         ipsum_ninja.write(''.join(ninja_text)* '\n\n')
